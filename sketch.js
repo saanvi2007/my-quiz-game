25a9461dd0cf5a972bd653e9a79e228ce6fe1a5b
saanvi2007 <72740918+saanvi2007@users.noreply.github.com>
2021-07-31 18:32:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas;
+var gameState=0;
+var contestantCount,database,quiz,question,contestant;
+function setup(){
+  canvas = createCanvas(850,400);
+  database=firebase.database()
+  quiz=new Quiz()
+  quiz.getState()
+  quiz.start()
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
